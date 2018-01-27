91b81bafe3bc67296dd3b354e803c5fe2022d9d9
Maksym <serhieiev@gmail.com>
2018-01-27 19:26:48 +0200

    Move unit tests into a folder with single file

--- lists/tests/test_all.py
+++ lists/tests/test_all.py
@@ -14,7 +14,7 @@ class HomePageTest(TestCase):
   def test_home_page_returns_correct_html(self):
     response = self.client.get('/')
     html = response.content.decode('utf8')
-    self.assertTrue(html.startswith('<html>'))
+    self.assertTrue(html.startswith('<!DOCTYPE html>'))
     self.assertIn('<title>To-Do lists</title>', html)
     self.assertTrue(html.strip().endswith('</html>'))
 
