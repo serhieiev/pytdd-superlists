ad7815b7fe7669096b2e527f7c3752483809e065
Maksym <serhieiev@gmail.com>
2018-01-27 11:23:31 +0200

    Moved FTs into their own individual files

--- functional_tests/test_simple_list_creation.py
+++ functional_tests/test_simple_list_creation.py
@@ -1,37 +1,11 @@
-from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from .base import FunctionalTest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from selenium.common.exceptions import WebDriverException
-import time
-import os
 
-MAX_WAIT = 10
 
-class NewVisitorTest(StaticLiveServerTestCase):
+class NewVisitorTest(FunctionalTest):
 
-  def setUp(self):
-    self.browser = webdriver.Chrome()
-    staging_server = os.environ.get('STAGING_SERVER')
-    if staging_server:
-        self.live_server_url = 'http://' + staging_server
-
-  def tearDown(self):
-    self.browser.quit()
-
-  def wait_for_row_in_list_table(self, row_text):
-    start_time = time.time()
-    while True:
-        try:
-            table = self.browser.find_element_by_id('id_list_table')
-            rows = table.find_elements_by_tag_name('tr')
-            self.assertIn(row_text, [row.text for row in rows])
-            return
-        except (AssertionError, WebDriverException) as e:
-            if time.time() - start_time > MAX_WAIT:
-                raise e
-            time.sleep(0.5)
-
-  def test_can_start_a_list_and_retrive_it_later(self):
+  def test_can_start_a_list_for_one_user(self):
     # Edith has heard about a cool new online to-do app. She goes
     # to check out its homepage
     self.browser.get(self.live_server_url)
@@ -62,7 +36,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
     inputbox = self.browser.find_element_by_id('id_new_item')
     inputbox.send_keys('Use peacock feathers to make a fly')
     inputbox.send_keys(Keys.ENTER)
-    time.sleep(1)
+
 
     # The page updates again, and now shows both items on her list
     table = self.browser.find_element_by_id('id_list_table')
@@ -77,31 +51,6 @@ class NewVisitorTest(StaticLiveServerTestCase):
     # She visits that URL - her to-do list is still there.
 
     # Satisfied, she goes back to sleep
-
-  def test_layout_and_styling(self):
-    # Edith goes to the home page
-    self.browser.get(self.live_server_url)
-    self.browser.set_window_size(1024, 768)
-
-    # She notices the input box is nicely centered
-    inputbox = self.browser.find_element_by_id('id_new_item')
-    self.assertAlmostEqual(
-        inputbox.location['x'] + inputbox.size['width'] / 2,
-        512,
-        delta=10
-        )
-
-    # She starts a new list and sees the input is nicely
-    # centered there too
-    inputbox.send_keys('testing')
-    inputbox.send_keys(Keys.ENTER)
-    self.wait_for_row_in_list_table('1: testing')
-    inputbox = self.browser.find_element_by_id('id_new_item')
-    self.assertAlmostEqual(
-        inputbox.location['x'] + inputbox.size['width'] / 2,
-        512,
-        delta=10
-        )
 
   def test_multiple_users_can_start_lists_at_different_urls(self):
     # Edith starts a new list
@@ -148,5 +97,6 @@ class NewVisitorTest(StaticLiveServerTestCase):
     self.assertIn('Buy milk', page_text)
 
     # Satisfied, they both go back to sleep
+
 
 
